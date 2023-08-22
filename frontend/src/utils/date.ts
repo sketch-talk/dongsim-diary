f7963feefa8dd68ae815ac5f576c7a71581bb893
gyeongza <gyeongza@naver.com>
2023-08-22 19:35:06 +0900

    refactor: 날짜 형식 유틸로 분리

---
+++ frontend/src/utils/date.ts
@@ -0,0 +1,6 @@
+export const today = new Date();
+export const getYear = today.getFullYear();
+export const getMonth = today.getMonth() + 1;
+export const getDate = today.getDate();
+export const getDay = today.getDay();
+export const day = ['일', '월', '화', '수', '목', '금', '토'];
