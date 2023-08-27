1e7da0c1e9b8cd2d34ad644f1dc4f78dbf398fa8
에이든 <62369936+gyeongza@users.noreply.github.com>
2023-08-27 18:51:41 +0900

    vite 정적 에셋 참조 추가 (#16)
    * feat: 정적 에셋 참조 추가
    
    * fix: 사용하지 않는 파라미터 삭제

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -2,11 +2,11 @@ import { rest } from 'msw';
 import imageUrl from './imageUrl.json';
 
 export const handlers = [
-  rest.post('*/post/contents', (req, res, ctx) => {
+  rest.post('*/post/contents', (_, res, ctx) => {
     return res(ctx.delay(1000), ctx.status(201));
   }),
 
-  rest.get('*/post/imageUrl', (req, res, ctx) => {
+  rest.get('*/post/imageUrl', (_, res, ctx) => {
     return res(ctx.delay(1000), ctx.status(200), ctx.json(imageUrl));
   }),
 ];
