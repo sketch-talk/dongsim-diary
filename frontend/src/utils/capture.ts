946ea45978a5abdaaa9858e4c31d0d26fcd98b05
gyeongza <gyeongza@naver.com>
2023-09-17 01:38:03 +0900

    chore: 사용하지 않는 라이브러리 삭제 및 @types 추가

--- frontend/src/utils/capture.ts
+++ frontend/src/utils/capture.ts
@@ -1,21 +1,3 @@
-// import * as htmlToImage from 'html-to-image';
-
-// export const capture = async (ref: React.RefObject<HTMLDivElement>) => {
-//   if (ref.current) {
-//     await htmlToImage
-//       .toPng(ref.current, { backgroundColor: '#ffffff' })
-//       .then((dataUrl) => {
-//         const link = document.createElement('a');
-//         link.download = '동심일기.png';
-//         link.href = dataUrl;
-//         link.click();
-//       })
-//       .catch((error) => {
-//         console.error('화면 캡쳐 중 오류가 발생했습니다:', error);
-//       });
-//   }
-// };
-
 import html2canvas from 'html2canvas';
 
 export const capture = async (ref: React.RefObject<HTMLDivElement>) => {
@@ -37,3 +19,21 @@ export const capture = async (ref: React.RefObject<HTMLDivElement>) => {
     }
   }
 };
+
+// import * as htmlToImage from 'html-to-image';
+
+// export const capture = async (ref: React.RefObject<HTMLDivElement>) => {
+//   if (ref.current) {
+//     await htmlToImage
+//       .toPng(ref.current, { backgroundColor: '#ffffff' })
+//       .then((dataUrl) => {
+//         const link = document.createElement('a');
+//         link.download = '동심일기.png';
+//         link.href = dataUrl;
+//         link.click();
+//       })
+//       .catch((error) => {
+//         console.error('화면 캡쳐 중 오류가 발생했습니다:', error);
+//       });
+//   }
+// };
