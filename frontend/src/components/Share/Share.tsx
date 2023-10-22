164518b59f4f7664f68d03da725e2a74060f19c2
gyeongza <gyeongza@naver.com>
2023-10-22 22:50:19 +0900

    assets: 공유하기 아이콘 변경

--- frontend/src/components/Share/Share.tsx
+++ frontend/src/components/Share/Share.tsx
@@ -1,26 +1,22 @@
-// import { ReactComponent as Facebook } from '../../assets/facebook-icon.svg';
-// import { ReactComponent as KakaoTalk } from '../../assets/kakao-icon.svg';
-// import { ReactComponent as Instagram } from '../../assets/insta-icon.svg';
+import { ReactComponent as KakaoTalk } from '../../assets/kakao_icon.svg';
+import { ReactComponent as Instagram } from '../../assets/instagram_icon.svg';
 import { styled } from 'styled-components';
 import { createPortal } from 'react-dom';
 
-interface Props {
-  handleCapture: () => void;
-  // handleShare: () => void;
-}
+const Share = () => {
+  const handleShare = () => {
+    alert('아직 준비중인 기능이에요🥲');
+  };
 
-const Share = ({ handleCapture }: Props) => {
   return createPortal(
     <S.ShareContainer>
       <S.ShareIconContainer>
-        <S.ReWriteButton href="/">다시 쓰기</S.ReWriteButton>
-        <S.SaveButton onClick={handleCapture}>이미지로 저장하기</S.SaveButton>
-        {/* <S.ShareIcon>
+        <S.ShareIcon>
           <p>내 동심일기 공유하기</p>
+          <KakaoTalk onClick={handleShare} width="33px" height="33px" />
           <Instagram onClick={handleShare} width="33px" height="33px" />
-          <Facebook onClick={handleShare} width="31px" height="31px" />
-          <KakaoTalk onClick={handleShare} width="30px" height="30px" />
-        </S.ShareIcon> */}
+        </S.ShareIcon>
+        <S.ReWriteButton href="/">다시 쓰기</S.ReWriteButton>
       </S.ShareIconContainer>
     </S.ShareContainer>,
     document.getElementById('share-root') as HTMLDivElement
@@ -39,7 +35,9 @@ const S = {
   ShareIconContainer: styled.div`
     display: flex;
     justify-content: center;
-    gap: 15px;
+    align-items: center;
+    flex-direction: column;
+    gap: 24px;
 
     margin: 40px 10px;
   `,
